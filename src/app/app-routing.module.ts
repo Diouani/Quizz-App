cc6538233b67c634684a0466e92b9681edd64369
Diouani <diouani.youssef@gmail.com>
2022-04-20 00:49:21 +0000

    mise en place de quelques components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { QuestionComponent } from './question/question.component';
+import { HomeComponent } from './home/home.component';
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path:"", redirectTo:"home", pathMatch:"full"},
+  {path:"home", component:HomeComponent},
+  {path:"question", component:QuestionComponent}
+
+
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
