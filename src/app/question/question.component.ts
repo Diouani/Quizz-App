87d72e41d73c5d93db242d6cedffc6c6bd07c2cd
Diouani <diouani.youssef@gmail.com>
2022-04-24 22:47:44 +0000

    ajout du system de niveau

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -12,53 +12,60 @@ export class QuestionComponent implements OnInit {
   public questionList: any = [];
   public currentIndex: number = 0;
   public points: number = 0;
-  public timeLeft = 60;
+  public timeLeft = 10;
   public correctAnswer = 0;
-  public  interval : any;
+  public interval: any;
+  public level: number = 1;
+  [key: string]: any;
 
   constructor(private QuestionService: QuestionService) {}
 
   ngOnInit(): void {
     this.pseudo = localStorage.getItem("pseudo")!;
-    this.getAllQuestions();
+    this.getAllQuestions(this.level);
     this.startTimer();
   }
 
-  getAllQuestions() {
+  getAllQuestions(level: number) {
+    this.currentIndex = 0;
     this.QuestionService.getQuestions().subscribe((res) => {
-      this.questionList = res.questions;
+      this.questionList = res["level" + level];
     });
   }
 
   skipQuestion() {
-    if(this.currentIndex == this.questionList.length -1 ){
-      alert("Correct Answer " + this.correctAnswer );
-      this.pauseTimer();
-
-    }else{
+    if (this.currentIndex == this.questionList.length - 1) {
+      if (this.correctAnswer >= 2) {
+        this.level = 2;
+        this.getAllQuestions(this.level);
+      } else if (this.level == 2 && this.correctAnswer >= 3) {
+        this.level = 3;
+        this.getAllQuestions(this.level);
+      } else if (this.level == 3 && this.correctAnswer >= 4) {
+        alert("You WIN");
+      }
+    } else {
       this.currentIndex = this.currentIndex + 1;
     }
-   
-  
   }
 
-  reponse(questionIndex :number , option : any){
-    if(option.bonneReponse) {
-      this.points += 10;
+  reponse(option: any) {
+    if (option.bonneReponse) {
+      this.points += 20;
       this.correctAnswer++;
     }
     this.skipQuestion();
-
   }
 
   startTimer() {
     this.interval = setInterval(() => {
-      if(this.timeLeft > 0) {
+      if (this.timeLeft > 0) {
         this.timeLeft--;
       } else {
-        this.timeLeft = 60;
+        this.skipQuestion();
+        this.timeLeft = 10;
       }
-    },1000)
+    }, 1000);
   }
 
   pauseTimer() {
